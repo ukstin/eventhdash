2101c4c852da658fc1f8005a86b3e7090d05ee54
ukstin <ukstin@gmail.com>
2021-06-10 14:11:54 -0300

    Alarme DAO

--- src/main/java/br/com/ukstin/eventdash/dao/AlarmeDAO.java
+++ src/main/java/br/com/ukstin/eventdash/dao/AlarmeDAO.java
@@ -5,6 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import br.com.ukstin.eventdash.model.Alarme;
 
 public interface AlarmeDAO extends CrudRepository<Alarme, Integer>{
-	public Alarme findByNome(String nome);
 
 }
